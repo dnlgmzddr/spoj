e55d9e5e51ed80c5bee4ef34a6194cf1e33d702c
Daniel Gomez <dgomezdi@outlook.com>
2018-11-12 20:47:23 -0500

    Add 001

---
+++ 001/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _001
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (string line; (line = Console.ReadLine()) != "42";)
+                Console.WriteLine(line);
+        }
+    }
+}
+\
